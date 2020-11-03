99ddca8d772ccb08fd17b6474e49bfbbb5cdec9f
admin <feelinblue@t-online.de>
2020-11-03 23:19:23 +0100

    all new stuff

---
+++ ollis_first_tries/4. Erste Datentypen/4.1.py
@@ -0,0 +1,33 @@
+# %%
+# Zahl durch die andere Teilbar - 5 und 4 --> Rest eins
+# %%
+x = 5
+y = 4
+
+
+x % y
+
+-x
+# hochschreiben!
+x**y
+# %%
+# floor division
+# division und rundung auf ganze zahl
+x = 15
+y = 2
+x // y
+
+# %%
+0.1 + 0.1 + 0.1 - 0.3
+
+# %%
+5 + 10**40 - 10**40
+# %%
+5.0 + 10**40 - 10**40
+# %%
+string_eins = "Hallo schöne, neue Welt"
+
+# %%
+string_eins.capitalize()
+
+# %%
