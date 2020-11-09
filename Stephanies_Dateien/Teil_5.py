ebcdcf2e0dc41536b6e5cbd334c58937eba1bb33
runcorner <srenneke@posteo.de>
2020-11-09 21:40:49 +0100

    Jetzt aber

--- Stephanies_Dateien/Teil_5.py
+++ Stephanies_Dateien/Teil_5.py
@@ -1,1 +1,5 @@
 
+#5.1.1 Aufgabe
+animals = ["tiger", "mouse", "bird", "python", "elephant", "monkey"]
+for animals in ["tiger", "mouse", "bird", "python", "elephant", "monkey"]:
+    print(animals "ist ein Tier")
