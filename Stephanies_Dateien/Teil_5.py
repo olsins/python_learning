ac71dcc0ea5b63268b230ccdb1965f29ae422d40
admin <feelinblue@t-online.de>
2020-11-09 21:42:48 +0100

    tier workinh

--- Stephanies_Dateien/Teil_5.py
+++ Stephanies_Dateien/Teil_5.py
@@ -1,5 +1,9 @@
 
-#5.1.1 Aufgabe
+# 5.1.1 Aufgabe
 animals = ["tiger", "mouse", "bird", "python", "elephant", "monkey"]
-for animals in ["tiger", "mouse", "bird", "python", "elephant", "monkey"]:
-    print(animals "ist ein Tier")
+
+for tier in animals:
+    print(tier + "ist ein Tier")
+
+# for animals in ["tiger", "mouse", "bird", "python", "elephant", "monkey"]:
+#    print(animals "ist ein Tier")
