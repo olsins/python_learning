451bcff723b1eb86fa62e7b44a94a31761470222
runcorner <srenneke@posteo.de>
2021-02-02 23:18:01 +0100

    weiterentwickelt

--- .vs/python_learning/Nilla/Nillas_first.py
+++ .vs/python_learning/Nilla/Nillas_first.py
@@ -2,6 +2,32 @@
 # Nillas Abendteuer
 
 #Startvariablen
+
+class room:
+    def __init__(self, name, description,):
+        self.name = name
+        self.description = description
+        self.exits = {}
+
+#define all rooms:
+berg = room("Berg",
+    "Der höchste alle Berge war seit Jahrtausenden Heimat der Zwerg*innen. Auf seiner"
+    "Spitze herrschten -100°C.")
+wald = room("Wald", 
+    "Im alten Wald ächzente die Bäume in Wind und das Sonnenlicht tanzte durch die Blätter.")
+meer = room("Meer",
+    "Das große, unendliche Meer in dessen Tiefen Ungeheuer, aber auch Abendteuer und"
+    "neue Freundschaften warteten.")
+wueste = room("Wüste",
+    "Die Luft flimmerte in der Hitze, die Tagsüber in der Wüste herrschte und Nachts war"
+    "die Kälte so schlimm, dass sich sogar Kakteen nach ein wenig Nähe und Wärme sehnten.")
+gebirge = room("Gebirge",
+    "Die großen Berge ragten wie Riesen (zwinker smiley) vor Dir auf.")
+stadt = room("Stadt",
+    "Die alte Stadt war einst ein Handelsknotenpunkt zwischen all den verschiedenen"
+    "Landschaften und ihren Bewohner*innen gewesen und lag nun schon seit vielen Jahren"
+    "in einem Zauberschlaf.")
+
 # 4 Richtungsknöpfe
 u = 'unten'
 o = 'oben'
