55ca92e1afe5fbb478db56ddf274dada2f94708b
admin <feelinblue@t-online.de>
2021-03-17 11:59:36 +0100

    push it all

--- Nilla/food_2.py
+++ Nilla/food_2.py
@@ -1,17 +1,17 @@
-class Person:  
+class Person:
     def __init__(self, name, age, favorite_foods):
         self.name = name
-        self. age = age
+        self.age = age
         self.favorite_foods = favorite_foods
-    
+
     def __str__(self):
         return "Name: " + self.name \
             + "Age: " + str(self.age) \
             + " Favorite food: " + str(self.favorite_foods[0])
 
-people = [Person("Ed", 11, ["hotdogs", "jawbreakers"])
-            , Person("Edd", 11, ["broccoli"])
-            , Person("Eddy", 12, ["chunky puffs", "jawbreakers"])]
+
+people = [Person("Ed", 11, ["hotdogs", "jawbreakers"]), Person(
+    "Edd", 11, ["broccoli"]), Person("Eddy", 12, ["chunky puffs", "jawbreakers"])]
 
 for person in people:
     print(person)
--- ollis_first_tries/Nillas Tries/food.py
+++ ollis_first_tries/Nillas Tries/food.py
@@ -1,3 +1,19 @@
+class Recipe:
+    def __init__(self, name, ingredients):
+        self.name = name
+        self.ingredients = ingredients
+
+    def __str__(self):
+        return "name: " + self.name \
+            + " calories: " + str(self.total_calories())
+
+    def total_calories(self):
+        all_calories = 0
+        for y in self.ingredients:
+            all_calories = y.calories() + all_calories
+        return all_calories
+
+
 class Food:
     def __init__(self, name, carbs, protein, fat):
         self.name = name
@@ -5,19 +21,29 @@ class Food:
         self.protein = protein
         self.fat = fat
 
+    def calories(self):
+        return (self.carbs*4) + (self.protein*4) + (self.fat*9)
+
     def __str__(self):
         return "Name: " + self.name \
-            + " Carbs " + self.carbs \
-            + " protein " + self.protein \
-            + " fat " + self.fat
+            + " Carbs " + str(self.carbs) \
+            + " protein " + str(self.protein) \
+            + " fat " + str(self.fat) \
+            + " calories: " + str(self.calories())
 
 
 # dishes = [Food(), Food()]
-dishes = [Food("lasagne", "11", "12", "13"), Food("chicken", "1", "5", "7")]
+dishes = [Food("lasagne", 11, 12, 13),
+          Food("chicken", 1, 5, 7)]
 
+
+recipe = [Recipe("crazyfood", dishes)]
 
 # print(dishes)
 for x in dishes:
     print(x)
 
-# print(dishes)
+# print(recipe)
+
+for x in recipe:
+    print(x)
